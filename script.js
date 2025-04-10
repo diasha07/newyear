420fa010c2889f27e3c29e9881d4f3a827f505b0
diasha07 <dianaschander07@gmail.com>
2025-04-10 20:59:44 +0300

    Add files via upload
    Signed-off-by: diasha07 <dianaschander07@gmail.com>

---
+++ script.js
@@ -0,0 +1,57 @@
+let kolvo = 0
+function fgift(a){
+if(a==1){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift1').style.display = "none";
+}
+if(a==2){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift2').style.display = "none";
+}
+if(a==3){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift3').style.display = "none";
+}
+if(a==4){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift4').style.display = "none";
+}
+if(a==5){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift5').style.display = "none";
+}
+if(a==6){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift6').style.display = "none";
+}
+if(a==7){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift7').style.display = "none";
+}
+if(a==8){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift8').style.display = "none";
+}
+if(a==9){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift9').style.display = "none";
+}
+if(a==10){
+    kolvo=kolvo+1
+    document.getElementById('kol').innerHTML=kolvo
+    document.querySelector('.gift10').style.display = "none";
+}
+if(kolvo==10){
+    alert("You won!🎉🎉🎉")
+}
+}
+
